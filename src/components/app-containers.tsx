8e85430102c25a45b7de0efba53de916e8c07ca2
mythily-muthu <mythilimuthu18@gmail.com>
2023-02-06 18:10:28 +0530

    Feat: removed unwanted files

---
+++ src/components/app-containers.tsx
@@ -0,0 +1,13 @@
+import header from "../layouts/header";
+
+export const AppContainer = () => {
+  return (
+    <>
+      {/*Header */}
+
+      {/*Body */}
+
+      {/*Footer */}
+    </>
+  );
+};
